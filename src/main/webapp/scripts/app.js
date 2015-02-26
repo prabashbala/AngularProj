d4e89afc6f9e868c75f5f431ad690fd3b740f98c
Prabash Balasuriya <prabashbala@yahoo.com>
2015-02-26 12:15:39 +0000

    refactroing code : add single factory to return singleton service to make ajax calls

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -2,13 +2,13 @@
  * Created by Prabash Balasuriya on 25/02/2015.
  */
 
-var app = angular.module('displayProjects', ['ngRoute']);
+var app = angular.module('displayProjects',[]);
 
 app.factory('dataService', function($http) {
 	return {
-		getProjects : function() {
+		getProjects :  function(input) {
 			// return the promise directly.
-			return $http.get('getProjects').then(function(result) {
+			return $http.get(input).then(function(result) {
 				// resolve the promise as the data
 				return result.data;
 			});
@@ -16,47 +16,26 @@ app.factory('dataService', function($http) {
 	}
 });
 
-app.factory('dataService2', function($http) {
-	return {
-		getProjects : function(input) {
-			// return the promise directly.
-			return $http.get('getProject/'+input).then(function(result) {
-				// resolve the promise as the data
-				return result.data;
-			});
-		}
-	}
-});
-
-app.factory('dataService3', function($http) {
-	return {
-		getProjects : function(input) {
-			// return the promise directly.
-			return $http.get('getIssue/'+input).then(function(result) {
-				// resolve the promise as the data
-				return result.data;
-			});
-		}
-	}
-});
-
-app.controller('displayProjectsController', function($scope,$location, dataService,dataService2,dataService3) {
-	dataService.getProjects().then(function(results) {
+app.controller('displayProjectsController', function($scope,dataService) {
+	//get projects
+	dataService.getProjects('getProjects').then(function(results) {
 		$scope.projects = results;
 	});
-	$scope.tab=1;
+
 	$scope.selectTab=function(setTab) {	
 		$scope.tab = setTab;
 		if(typeof setTab.fields === 'undefined'){
 		
-			dataService2.getProjects(setTab.id).then(function(results) {
+			dataService.getProjects('getProject/'+setTab.id).then(function(results) {
 				$scope.projects = results.issues;
 			});
 		
-		}else {
-		
-			dataService3.getProjects(setTab.id).then(function(results) {
-				$scope.projects = results.issuetype;
+		}else {		
+			dataService.getProjects('getIssue/'+setTab.id).then(function(results) {
+			//alert(results.fields.issuetype.description);
+			var empty;
+				$scope.projects =empty;
+				$scope.issuedetails = results;
 			});
 		}
 		
