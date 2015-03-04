22dd8040036bb5c3cbbbf0ce004912cf4a2ba686
Prabash Balasuriya <prabashbala@yahoo.com>
2015-03-04 10:09:49 +0000

    add banner and use angularUtils.directives.uiBreadcrumbs plugin for breadcrumbs

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -2,13 +2,13 @@
  * Created by Prabash Balasuriya on 25/02/2015.
  */
 
-var app = angular.module('displayProjects',['ngRoute']);
+var app = angular.module('displayProjects', ['ui.router', 'angularUtils.directives.uiBreadcrumbs']);
 
-app.factory('dataService', function($http) {
+app.factory('dataService', function ($http) {
 	return {
-		getProjects :  function(input) {
+		getProjects : function (input) {
 			// return the promise directly.
-			return $http.get(input).then(function(result) {
+			return $http.get(input).then(function (result) {
 				// resolve the promise as the data
 				return result.data;
 			});
@@ -16,66 +16,65 @@ app.factory('dataService', function($http) {
 	}
 });
 
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-	 when('/', {
-        templateUrl: 'templates/show_projects.html',
-        controller: 'displayProjectsController'
-      }).
-      when('/projects', {
-        templateUrl: 'templates/show_projects.html',
-        controller: 'displayProjectsController'
-      }).
-      when('/issueslist/:projectid', {
-        templateUrl: 'templates/show_issueslist.html',
-        controller: 'displayIssuesListController'
-      }).
-	  when('/issue/:issueid', {
-        templateUrl: 'templates/show_issue.html',
-        controller: 'displayIssueController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
-
-app.controller('displayProjectsController', function($scope,$location,dataService) {
-	//get projects
-	$scope.isProjectsloaded=false;	
-	dataService.getProjects('getProjects').then(function(results) {
+app.controller('displayProjectsController', function ($scope, $location, dataService) {
+	dataService.getProjects('getProjects').then(function (results) {
 		$scope.projects = results;
 	});
 
-	$scope.selectTab=function(setTab) {	
-		$scope.tab = setTab;		
-		$location.path("/issueslist/"+$scope.tab.id);	
-		$scope.isProjectsloaded=true;		
-	  };	  
-	  $scope.isSelected=function(checkTab) {	
-		return $scope.tab === checkTab;
-	  };
-	  
+	$scope.selectTab = function (setTab) {
+		$scope.tab = setTab;
+		$location.path("/project/" + $scope.tab.id);
+	};
 });
 
-app.controller('displayIssuesListController', function($scope,$location,$routeParams,dataService) {
-	dataService.getProjects('getProject/'+$routeParams.projectid).then(function(results) {
+app.controller('displayIssuesListController', function ($scope, $location, $stateParams, dataService) {
+
+	dataService.getProjects('getProject/' + $stateParams.projectid).then(function (results) {
 		$scope.issuelist = results.issues;
-	});	
-	
-	$scope.selectIssueTab=function(setTab) {	
-		$scope.issuetab = setTab;	
-		$location.path("/issue/"+$scope.issuetab.id);				
-	  };
-	  
-	  $scope.isIssueSelected=function(checkTab) {	
-		return $scope.issuetab === checkTab;
-	  };	
+	});
+
+	$scope.selectIssueTab = function (setTab) {
+		$scope.issuetab = setTab;
+		$location.path("/issue/" + $scope.issuetab.id);
+	};
 });
 
-app.controller('displayIssueController', function($scope,$routeParams,dataService) {
-	dataService.getProjects('getIssue/'+$routeParams.issueid).then(function(results) {
+app.controller('displayIssueController', function ($scope, $stateParams, dataService) {
+	dataService.getProjects('getIssue/' + $stateParams.issueid).then(function (results) {
 		$scope.issuedetails = results;
-	});	
+	});
 });
 
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+	//
+	// For any unmatched url, redirect to /state1
+	$urlRouterProvider.otherwise("/home");
+	//
+	// Now set up the states
+	$stateProvider
+	.state('home', {
+		url : "/home",
+		templateUrl : 'templates/show_projects.html',
+		controller : 'displayProjectsController',
+		data : {
+			displayName : 'Home',
+		}
+	}).state('home.project', {
+		url : "^/project/:projectid",
+		template : '',
+		templateUrl : 'templates/show_issueslist.html',
+		controller : 'displayIssuesListController',
+		data : {
+			displayName : 'Project',
+		}
+	}).state('home.project.issue', {
+		url : "^/issue/:issueid",
+		template : '',
+		templateUrl : 'templates/show_issue.html',
+		controller : 'displayIssueController',
+		data : {
+			displayName : 'Issue',
+		}
+	});
+});
